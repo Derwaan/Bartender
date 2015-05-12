247b4bb70d6193e8d40899d179e224c3ea95d5f4
Qsaws <qsaws@Qsaws-Crunchbang.(none)>
2015-05-12 16:06:13 +0200

    ajout du début de product.java

---
+++ Application/app/src/main/java/m/groupe/bartender/model/Product.java
@@ -0,0 +1,44 @@
+package m.groupe.bartender;
+
+/**
+ * Product construit un objet
+ *
+ * @author Romain Leonard (romain.leonard@student.uclouvain.be)
+ * @version 0.1
+ */
+public class Product
+{PRIMARY KEY (ID_PROD)
+    protected int id_prod;
+    protected int descr;
+    protected int nom_prod;
+    protected int catego;
+    protected double prix_prod;
+    protected int seuil_prod;
+    protected int primary_key;
+
+    /**
+     *  /// Constructor ///
+     *
+     * @pre : word != null
+     * @post : Construit un objet de type Word contenant un String et un int
+     */
+    public Product(String word , int instance)
+    {
+        assert word != null : "word ne peut etre null, voir pre";
+        this.id_prod;
+        this.descr;
+        this.nom_prod;
+        this.catego;
+        this.prix_prod;
+        this.seuil_prod;
+        this.primary_key;
+    }
+
+    /**
+     *@pre : /
+     *@post : retourne l'id de la description de ce produit
+     */
+    public int getDescr()
+    {
+        return this.descr;
+    }
